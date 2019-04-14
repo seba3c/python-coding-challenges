23a6b5a1ee2c5bc76395f9bfffaafe3f9229ebf0
seba3c <castanedacs@gmail.com>
2019-04-13 23:29:59 -0300

    Fixed queens solution

--- src/queens.py
+++ src/queens.py
@@ -4,13 +4,23 @@ from ast import literal_eval
 
 
 def queens_without_collisions(queens_positions=[]):
+
+    def has_collition(point_1, point_2):
+        x1, y1 = point_1[0], point_1[1]
+        x2, y2 = point_2[0], point_2[1]
+        # check if the points are:
+        # 1 - horizontally aligned
+        # 2 - vertically aligned
+        # 3 - form a 45 degree angle
+        return x1 == x2 or y1 == y2 or abs(x1 - x2) == abs(y1 - y2)
+
     queens_positions = [literal_eval(pos) for pos in queens_positions]
     for i in range(0, len(queens_positions)):
         for j in range(i + 1, len(queens_positions)):
-            i_pos = queens_positions[i]
-            j_pos = queens_positions[j]
-            if i_pos[0] == j_pos[0] or i_pos[1] == j_pos[1]:
-                return str(i_pos).replace(' ', '')
+            point_1 = queens_positions[i]
+            point_2 = queens_positions[j]
+            if has_collition(point_1, point_2):
+                return str(point_1).replace(' ', '')
     return str(True)
 
 
@@ -30,6 +40,10 @@ if __name__ == '__main__':
 
     Input = ["(4,1)", "(7,2)", "(3,3)", "(8,4)", "(2,5)", "(5,6)", "(1,7)", "(6,8)"]
     Output = "True"
+    assert queens_without_collisions(Input) == Output
+
+    Input = ["(4,1)", "(3,2)"]
+    Output = "(4,1)"
     assert queens_without_collisions(Input) == Output
 
     print("OK!")
